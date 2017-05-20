f13d30a0384684c4acf0483cacc4ab249c94ad63
Peter.w <peterwolf.wang@gmail.com>
2017-05-20 21:18:42 +0800

    新增左下 File List 區塊可以拖放檔案以新增項目的功能。

--- 2ndSense.py
+++ 2ndSense.py
@@ -13,11 +13,108 @@
 # Copyright 2016 Droidtown Ling. Tech. Co., Ltd
 # All Rights Reserved.
 
-
+import os
 import sys
 
 from PySide import QtCore
 from PySide import QtGui
+
+from urllib.parse import urlparse
+
+class TableWidgetItem(QtGui.QTableWidgetItem):
+    '''
+    在原始 QTableWidgetItem 類別下, 新增表格 item 用來排序的數值型態。
+    '''
+    def __init__(self, text, sortKey):
+        QtGui.QTableWidgetItem.__init__(self, text, QtGui.QTableWidgetItem.UserType)
+        self.sortKey = sortKey
+
+    #Qt uses a simple < check for sorting items, override this to use the sortKey
+    def __lt__(self, other):
+        return self.sortKey < other.sortKey
+
+
+
+class DropZoneLabel(QtGui.QLabel):
+    def __init__(self):
+        super(DropZoneLabel, self).__init__()
+
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, e):
+        print("dragging enter")
+        if e.mimeData().hasUrls:
+            e.accept()
+        else:
+            e.ignore()
+        #if e.mimeData().hasFormat('text/plain'):
+            #e.accept()
+        #else:
+            #e.ignore()
+
+    def dropEvent(self, e):
+        print("dropping at", e.mimeData())
+        if e.mimeData().hasUrls:
+            fileLIST = []
+            for url in e.mimeData().urls():
+                path = url.toLocalFile()
+                if os.path.isfile(path):
+                    fileLIST.append(path)
+            print(fileLIST)
+            return fileLIST
+        else:
+            return None
+
+class FileListTable(QtGui.QTableWidget):
+    def __init__(self):
+        super(FileListTable, self).__init__()
+        self.fileLIST = []
+        self.setAcceptDrops(True)
+        self.verticalHeader().setVisible(False)
+        self.horizontalHeader().setVisible(False)
+
+    def dragEnterEvent(self, e):
+        print("fileLIST dragging enter")
+        if e.mimeData().hasUrls:
+            e.accept()
+        else:
+            e.ignore()
+        #if e.mimeData().hasFormat('text/plain'):
+            #e.accept()
+        #else:
+            #e.ignore()
+
+    def dragMoveEvent(self, e):
+        e.accept()
+
+    def dropEvent(self, e):
+        print("fileLIST dropping at", e.mimeData())
+        if e.mimeData().hasUrls:
+            for url in e.mimeData().urls():
+                path = url.toLocalFile()
+                if os.path.isfile(path):
+                    self.fileLIST.append([path])
+            self.fileListSetter(self.fileLIST)
+        else:
+            return None
+
+    def fileListSetter(self, fileLIST):
+        print("fileLIST:", fileLIST)
+        self.insertRow(len(fileLIST))
+        self.setRowCount(len(self.fileLIST))
+        self.setColumnCount(len(self.fileLIST[0]))
+        self.setSortingEnabled(False)
+        for i, row in enumerate(sorted(fileLIST)):
+            for j, col in enumerate(row):
+                item = TableWidgetItem(col, col)
+                self.setItem(i, j, item)
+        self.setSortingEnabled(True)
+        self.resizeColumnsToContents()
+        #for f in fileLIST:
+            #self.setItem(0, 0, f)
+
+
+
 
 class MainWindow(QtGui.QWidget):
     def __init__(self):
@@ -61,16 +158,18 @@ class MainWindow(QtGui.QWidget):
         lowerLeftVBox.setContentsMargins(0, 0, 0, 0)
 
         # <內容>
-        upperLeftLabel = QtGui.QLabel(upperLeftFrame)
+        upperLeftLabel = DropZoneLabel() #QtGui.QLabel(upperLeftFrame)
         upperLeftLabel.setText(">Drop Zone<")
         upperLeftLabel.setAlignment(QtCore.Qt.AlignCenter)
         upperLeftLabel.setFixedSize(self.dropZoneWidth, self.dropZoneHeight)
         upperLeftVBox.addWidget(upperLeftLabel)
 
-        lowerLeftTable = QtGui.QTableWidget(lowerLeftFrame)
         lowerLeftLable = QtGui.QLabel()
         lowerLeftLable.setText("File List:")
         lowerLeftVBox.addWidget(lowerLeftLable)
+        #lowerLeftTable = QtGui.QTableWidget(lowerLeftFrame)
+        lowerLeftTable = FileListTable() #QtGui.QTableWidget(lowerLeftFrame)
+        #lowerLeftTable.setItem()
         lowerLeftVBox.addWidget(lowerLeftTable)
 
         upperRightButton = QtGui.QPushButton(upperRightFrame)
@@ -78,7 +177,7 @@ class MainWindow(QtGui.QWidget):
         upperRightVBox.addWidget(upperRightButton)
 
         lowerRightButton = QtGui.QPushButton(lowerRightFrame)
-        lowerRightButton.setText("RIGHT TOP")
+        lowerRightButton.setText("RIGHT Lower")
         lowerRightVBox.addWidget(lowerRightButton)
         # </內容>
 
@@ -99,11 +198,37 @@ class MainWindow(QtGui.QWidget):
         #screenSize = QtGui.QDesktopWidget().screenGeometry()
 
         #self.setGeometry(screenSize)
+        self.setFixedSize(1046, 550)
+        #self.setAcceptDrops(True)
+        extRightFrame.setAcceptDrops(False)
+
 
         self.show()
 
-def dropAndOpenEvent(self, e):
-    return None
+        return None
+
+    #def dragEnterEvent(self, e):
+        #print("dragging enter")
+        #if e.mimeData().hasUrls:
+            #e.accept()
+        #else:
+            #e.ignore()
+
+    def dragMoveEvent(self, e):
+        print("dragging move")
+        if e.mimeData().hasUrls:
+            e.accept()
+        else:
+            e.ignore()
+
+    #def dropEvent(self, e):
+        #print("dropping at", e.mimeData())
+        ##print((10, 10) < e.pos())
+        ##if (10, 10) < e.pos() < (self.dropZoneWidth, self.dropZoneHeight):
+            ##print("Inside drop zone")
+        #if e.mimeData().hasUrls:
+            #print(e.mimeData().urls())
+        #return None
 
 
 def main():
