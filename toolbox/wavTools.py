62f954509e03f2e482d86bc26f584743be37643a
Peter.w <peterwolf.wang@gmail.com>
2017-05-24 06:43:23 +0800

    新增 wavTools.py > WavGear 讀取 .wav 檔功能；新增 400_sine.wav 測試用 .wav 音檔。

--- toolbox/wavTools.py
+++ toolbox/wavTools.py
@@ -1,14 +1,56 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# FileName:
-# Development starting date:
-# Modification date log    :
-# Last modification date   : 
-# Developer: Peter. w (peterwolf.wang@gmail.com)
-# This Software/Scipt is protected by copyright and other 
-# intellectual property laws and treaties. 
-# Droidtown Linguistic Technology Co., Ltd. owns the copyright, 
-# and other intellectual property rights in the Software/Script.
-# 卓騰語言科技有限公司 版權所有 
-# Copyright 2012 Droidtown Ling. Tech. Co., Ltd 
-# All Rights Reserved.
+# FileName: wavTools.py
+
+import numpy
+import wave
+
+class WavGear:
+    def __init__(self):
+        self.wavFileName = None
+        self.frameNumber = None
+        self.channelNumber = None
+        self.wavDataDICT = {"signal"           : None,
+                            "normalizedSignal" : None,
+                            "samplingRate"     : None,
+                            "width"            : None, # in bytes. 1->8 bits, 2->16 bits
+                            "env"              : None,
+                            }
+
+    def wavReader(self, wavFileName):
+        self.wavFileName = wavFileName
+
+        self.wavFileObj = wave.open(self.wavFileName, "r")
+        self.frameNumber = self.wavFileObj.getnframes()
+        self.wavDataDICT["signal"] = numpy.fromstring(self.wavFileObj.readframes(self.frameNumber), "Int16")/32767.0
+        self.wavDataDICT["normalizedSignal"] = self._signalNormalizer(self.wavDataDICT["signal"])
+        self.wavDataDICT["samplingRate"] = self.wavFileObj.getframerate()
+        self.wavDataDICT["width"] = self.wavFileObj.getsampwidth()
+        self.wavFileObj.close()
+        if self.wavDataDICT["width"] == 2:
+            self.wavDataDICT["env"] = "pcm16"
+        elif self.wavDataDICT["width"] == 1:
+            self.wavDataDICT["env"] = "pcm8"
+        else:
+            self.wavDataDICT["env"] = "Unknown. (Only 8bit or 16bit audio are supported."
+
+        return self.wavDataDICT
+
+    def _signalNormalizer(self, signalARY, ceilling=0.95):
+        avg = numpy.mean(signal)
+        wavSignalARY = signalARY - avg
+        normalizedSignalARY = numpy.int16(wavSignalARY/numpy.max(numpy.abs(wavSignalARY)*1.0)*32767*ceilling)
+        return normalizedSignalARY
+
+    def wavwrite(self):
+        print("--SndFreqTools wavwrite starts --")
+        self.wavFile.setparams((1, 2, 44100, len(self.signal), "NONE", "not compressed"))
+        data = []
+        for i in self.signal:
+            data.append(struct.pack('h', i*32768))
+        self.wavFile.writeframes("".join(data))
+        fileSize = round((len(self.signal)*2+44)/1024.0, 2)
+        self.wavFile.close()
+        print("--SndFreqTools wavwrite ends --")
+
+        return fileSize
