ff103ce3a2cf02a881c6b1997f797225108c25c6
Peter.w <peterwolf.wang@gmail.com>
2017-05-28 16:46:22 +0800

    移動 UI 元件到 UI 目錄下，以利長期分層開發。

--- UI/ctrlButtons.py
+++ UI/ctrlButtons.py
@@ -1,14 +1,44 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# FileName:
-# Development starting date:
-# Modification date log    :
-# Last modification date   : 
+# FileName:ctrlButtons.py
+
 # Developer: Peter. w (peterwolf.wang@gmail.com)
-# This Software/Scipt is protected by copyright and other 
-# intellectual property laws and treaties. 
-# Droidtown Linguistic Technology Co., Ltd. owns the copyright, 
+# This Software/Scipt is protected by copyright and other
+# intellectual property laws and treaties.
+# Droidtown Linguistic Technology Co., Ltd. owns the copyright,
 # and other intellectual property rights in the Software/Script.
-# 卓騰語言科技有限公司 版權所有 
-# Copyright 2012 Droidtown Ling. Tech. Co., Ltd 
+# 卓騰語言科技有限公司 版權所有
+# Copyright 2017 Droidtown Ling. Tech. Co., Ltd
 # All Rights Reserved.
+
+import os
+
+from PySide import QtGui
+
+class DropZoneLabel(QtGui.QLabel):
+    def __init__(self):
+        super(DropZoneLabel, self).__init__()
+        self.setToolTip("Drag & Drop files to add to the 'File list' below!")
+        self.setAcceptDrops(True)
+
+    def dragEnterEvent(self, e):
+        print("dragging enter")
+        if e.mimeData().hasUrls:
+            e.accept()
+        else:
+            e.ignore()
+
+    def dropEvent(self, e):
+        print("dropping at", e.mimeData())
+        if e.mimeData().hasUrls:
+            fileLIST = []
+            fileDICT = {}
+            for url in e.mimeData().urls():
+                path = url.toLocalFile()
+                if os.path.isfile(path):
+                    fileLIST.append(path)
+            print(fileLIST)
+
+            return fileLIST
+        else:
+            return None
--- UI/fileManager.py
+++ UI/fileManager.py
@@ -1,14 +1,92 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# FileName:
-# Development starting date:
-# Modification date log    :
-# Last modification date   : 
+# FileName: fileManager.py
+
 # Developer: Peter. w (peterwolf.wang@gmail.com)
-# This Software/Scipt is protected by copyright and other 
-# intellectual property laws and treaties. 
-# Droidtown Linguistic Technology Co., Ltd. owns the copyright, 
+# This Software/Scipt is protected by copyright and other
+# intellectual property laws and treaties.
+# Droidtown Linguistic Technology Co., Ltd. owns the copyright,
 # and other intellectual property rights in the Software/Script.
-# 卓騰語言科技有限公司 版權所有 
-# Copyright 2012 Droidtown Ling. Tech. Co., Ltd 
+# 卓騰語言科技有限公司 版權所有
+# Copyright 2012 Droidtown Ling. Tech. Co., Ltd
 # All Rights Reserved.
+
+import os
+
+from PySide import QtGui
+
+
+class TableWidgetItem(QtGui.QTableWidgetItem):
+    '''
+    在原始 QTableWidgetItem 類別下, 新增表格 item 用來排序的數值型態。
+    '''
+    def __init__(self, text, sortKey):
+        QtGui.QTableWidgetItem.__init__(self, text, QtGui.QTableWidgetItem.UserType)
+        self.sortKey = sortKey
+
+    #Qt uses a simple < check for sorting items, override this to use the sortKey
+    def __lt__(self, other):
+        return self.sortKey < other.sortKey
+
+class FileListTable(QtGui.QTableWidget):
+    def __init__(self):
+        super(FileListTable, self).__init__()
+        self.fileLIST = []
+        self.fileDICT = {}
+        self.toolTip = "Drag&Drop files here!"
+        self.currentHoverLIST = [0, 0]
+        self.setAcceptDrops(True)
+        self.verticalHeader().setVisible(False)
+        self.horizontalHeader().setVisible(False)
+        self.setShowGrid(False)
+
+    def dragEnterEvent(self, e):
+        print("fileLIST dragging enter")
+        if e.mimeData().hasUrls:
+            e.accept()
+        else:
+            e.ignore()
+        #if e.mimeData().hasFormat('text/plain'):
+            #e.accept()
+        #else:
+            #e.ignore()
+
+    def dragMoveEvent(self, e):
+        e.accept()
+
+    def dropEvent(self, e):
+        print("fileLIST dropping at", e.mimeData())
+        if e.mimeData().hasUrls:
+            for url in e.mimeData().urls():
+                path = url.toLocalFile()
+                if os.path.isfile(path):
+                    self.fileDICT[str(len(self.fileLIST)+1)] = {"fullPath": path, "fileObj":None} #filePath 用於讀取檔案； fileObj 用於記錄該檔案是否已開啟。若已實體化並開啟，就不會是 None
+                    self.fileLIST.append([str(len(self.fileLIST)+1), os.path.basename(path)])
+            self.fileListSetter(self.fileLIST)
+        else:
+            return None
+
+    def fileListSetter(self, fileLIST):
+        print("fileLIST:", fileLIST)
+        self.insertRow(len(fileLIST))
+        self.setRowCount(len(self.fileLIST))
+        self.setColumnCount(len(self.fileLIST[0]))
+        self.setSortingEnabled(False)
+        for i, row in enumerate(sorted(fileLIST)):
+            for j, col in enumerate(row):
+                item = TableWidgetItem(col, col)
+                item.setToolTip(self.fileDICT[str(i+1)]["fullPath"])
+                self.setItem(i, j, item)
+        self.setEditTriggers(QtGui.QAbstractItemView.NoEditTriggers)
+        self.setSortingEnabled(True)
+        self.hideColumn(0) #隱藏 self.fileDICT 的 key 值
+        self.resizeColumnsToContents()
+        return None
+
+    def rowHover(self, row, column):
+        currentItem  = self.item(row, column)
+        previousItem = self.item(self.currentHoverLIST[0], self.currentHoverLIST[1])
+        if self.currentHoverLIST == [row, column]:
+            pass
+        else:
+            currentItem.showhint
